26fb1e69b39a72f731e205b3ab80f1086f396fd2
DusanPajic002 <dusanpajic2002@gmail.com>
2023-11-14 20:02:58 +0100

    3

--- test.rb
+++ test.rb
@@ -3,32 +3,41 @@ require "google_drive"
 session = GoogleDrive::Session.from_config("config.json")
 spreadsheet = session.spreadsheet_by_key("1rwfAax11xRiqGEoKELU42pneJMoY-8fDmiIlVSXOcH8")
 worksheet = spreadsheet.worksheets[0]
-dvodimenzionalni_niz = worksheet.rows
+tabela_niz = worksheet.rows
 
 class Tabela
 
   include Enumerable
 
-  def initialize(table_data)
-    @vrednosti = table_data
+  def initialize(tabela)
+    @t = tabela
   end
 
-  def row(row_number)
-    @vrednosti[row_number - 1]
+  def row(row)
+    @t[row-1]
   end
 
   def each
-    @vrednosti.each do |red|
-      red.each do |element|
-        yield element
+    @t.each do |row|
+      row.each do |n|
+        yield n
       end
     end
   end
 
+  def [] (kolona)
+    k = @t.first.index(kolona)
+    raise "Kolona ne postoji" unless k
+
+    @t.drop(1).map{ |row| row[k]}
+  end
+
 end
 
-tabela = Tabela.new(dvodimenzionalni_niz)
+tabela = Tabela.new(tabela_niz)
 
-tabela.each do |element|
-  p element
-end
+# tabela.each do |n|
+#   p n
+# end
+
+p tabela["Prva Kolona"]
