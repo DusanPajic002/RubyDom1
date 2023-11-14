bc3e1418232595870e4ba7c4e8992acc1944bff0
DusanPajic002 <dusanpajic2002@gmail.com>
2023-11-14 16:57:35 +0100

    2

--- test.rb
+++ test.rb
@@ -1,11 +1,34 @@
 require "google_drive"
 
-# Creates a session. This will prompt the credential via command line for the
-# first time and save it to config.json file for later usages.
-# See this document to learn how to create config.json:
-# https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
 session = GoogleDrive::Session.from_config("config.json")
+spreadsheet = session.spreadsheet_by_key("1rwfAax11xRiqGEoKELU42pneJMoY-8fDmiIlVSXOcH8")
+worksheet = spreadsheet.worksheets[0]
+dvodimenzionalni_niz = worksheet.rows
 
-ws = session.spreadsheet_by_key("1rwfAax11xRiqGEoKELU42pneJMoY-8fDmiIlVSXOcH8").worksheets[0]
+class Tabela
 
-p ws[2, 1]  #==> "hoge"
+  include Enumerable
+
+  def initialize(table_data)
+    @vrednosti = table_data
+  end
+
+  def row(row_number)
+    @vrednosti[row_number - 1]
+  end
+
+  def each
+    @vrednosti.each do |red|
+      red.each do |element|
+        yield element
+      end
+    end
+  end
+
+end
+
+tabela = Tabela.new(dvodimenzionalni_niz)
+
+tabela.each do |element|
+  p element
+end
